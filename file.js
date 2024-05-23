f14d823a41a81759f70e611b4f1075ae3ef1b2a7
arunvishwas <arunvishwas96@gmail.com>
2024-05-23 12:47:29 +0530

    newcommit

--- file.js
+++ file.js
@@ -2,6 +2,10 @@ var a = +prompt("enter num1")
 var b = +prompt("enter num2") 
 console.log(a+b)
 
+<<<<<<< HEAD
 console.log(second commit) 
 console.log(third commit)
 console.log("forth commit")
+=======
+console.log(second commit)
+>>>>>>> parent of f9fba41 (third)
